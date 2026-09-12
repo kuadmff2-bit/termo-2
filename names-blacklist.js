75b1d3ec8a8071aa8d9cd351febcdb93bd145c46
kuadm <kuadmff2@gmail.com>
2026-09-11 22:27:42 -0300

    Filtra nomes próprios das respostas do TERMU

---
+++ names-blacklist.js
@@ -0,0 +1,31 @@
+(() => {
+  'use strict';
+
+  const names = [
+    'aaron','adail','adams','adela','adeli','adler','adolf','adrian','adria','agnes','ailton','alan','alana','alane','alani','alceu','alice','alina','aline','alison','allan','allen','almir','aluis','alves','amaro','ameli','amory','andre','andry','angel','anita','anton','ariel','arlen','arley','artur','aylla',
+    'barry','bento','betty','bianca','brady','breno','brian','bruna','bruno','byron',
+    'carla','carlo','carol','celio','celso','cesar','chris','clara','clark','craig','cyrus',
+    'dario','davi','david','derek','denis','diana','diego','dylan',
+    'edgar','edson','elias','elisa','elise','ellen','elton','erick','erica','erika','ester','ethan',
+    'fabio','felix','fiona','flora','frank','fredy',
+    'gavin','geisa','giuli','glory','grace','greta',
+    'helen','helio','heber','henry','hilda','higor',
+    'ilana','irene','isaac','italo','ivone',
+    'jacob','jaime','jairo','james','jason','jeane','jesus','joana','joice','jonas','jorge','julia','julio','junia',
+    'karen','karla','katia','katie','kauan','keila','kelly','kelma','kevin','klaus','kylie',
+    'laura','leila','lenon','leona','lidia','lilia','livia','logan','loren','louis','lucas','lucia','lucio','luisa','luiza',
+    'maira','maisa','marco','maria','mario','marta','mauro','mayra','megan','mirna','moira',
+    'nadia','natan','nilda','nilza','nivia','norma',
+    'odair','olavo','oscar','osmar','otelo',
+    'pablo','paula','paulo','pedro','peter','pietro',
+    'ramon','renan','renee','roque','ruben',
+    'sarah','saulo','selma','silas','simon','sofia','sonia','steve','susan',
+    'taina','tales','tania','tatia','tiago','tomas','tyler',
+    'valdo','vales','vania','vitor','vivia',
+    'wanda','wayne','wilma',
+    'yanna','yasna','yuri',
+    'zaque','zelia','zilda'
+  ];
+
+  window.TERMO_BANNED_NAMES = new Set(names);
+})();
